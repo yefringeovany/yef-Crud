fa95d6160d63c49549ed0da7a5deb4aecab97c38
yefringeovany <yperezm18@miumg.edu.gt>
2024-05-30 15:13:32 -0600

    cambios 1

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
 const connectToMongo = require('./db')
 connectToMongo();
 
-const express = require('express')
-const app = express()
-const port = 4000
-
-const cors = require('cors')
 const router = require('./Routes/router')
+
+const app = express()
 
 app.use(cors());
 app.use(express.json());
+
 app.use(router);
 
+const port = 4000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
